6494377c5ee62bb228f92bfffb4006b944caf298
Dmitry <dima.kornakow@yandeex.ru>
2025-09-03 17:57:14 +0300

    feat: добавлены заголовки к столбцам

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,13 +1,12 @@
 import { useState, type FormEvent } from "react";
 import useExpenseStore from "../store/useExpenseStore";
 import type { Category } from "../types";
-import '../styles/ExpenseForm.scss'
+import '../styles/ExpenseForm.scss';
 
 const ExpenseForm = () => {
-
   const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState<Category>('Еда');
-  const [description, setDescription] = useState('')
+  const [description, setDescription] = useState('');
   const addExpense = useExpenseStore(state => state.addExpense);
 
   const categories: Category[] = ['Еда', 'Транспорт', 'Развлечения', 'Связь', 'Жилье'];
@@ -16,39 +15,52 @@ const ExpenseForm = () => {
     e.preventDefault();
 
     if (amount <= 0) {
-      alert('Сумма должна быть больше 0')
+      alert('Сумма должна быть больше 0');
       return;
     }
 
-    addExpense({category, amount, description});
+    addExpense({ category, amount, description });
 
     setAmount(0);
-    setCategory('Еда')
-    setDescription('')
+    setCategory('Еда');
+    setDescription('');
   }
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input type="number"
-        placeholder="Введите сумму траты"
-        value={amount}
-        onChange={(e) => setAmount(parseFloat(e.target.value) || 0 )}
-        onFocus={(e) => e.target.select()}
-        />
-        <select
-          value={category}
-          onChange={(e) => setCategory(e.target.value as Category)}
-        >
-          {categories.map( (category) => (
-            <option key={category} value={category}>{category}</option>
-          ))}
-        </select>
-        <input type="text"
-        placeholder="Описание(необязательно)"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        />
+        <div className="form-control">
+          <label htmlFor="amount">Сумма</label>
+          <input 
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(parseFloat(e.target.value) || 0)}
+            onFocus={(e) => e.target.select()}
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="category">Категория</label>
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value as Category)}
+          >
+            {categories.map((category) => (
+              <option key={category} value={category}>{category}</option>
+            ))}
+          </select>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="description">Описание</label>
+          <input 
+            type="text"
+            placeholder="Необязательно"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        
         <button type="submit">Сохранить</button>
       </form>
     </>
--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -6,27 +6,34 @@ const ExpenseList = () => {
   const expenses = useExpenseStore(state => state.expenses)
   const deleteExpense = useExpenseStore(state => state.deleteExpense)
 
-return (
-  <>
-    <h1>История трат:</h1>
-    {expenses.length === 0 
-      ? (<p>Пока что трат не было</p>)
-      : (
-        <ul>
-          {expenses.map((expense) => (
-            <li key={expense.id}>
-              <p>{expense.category}</p>
-              <p>{expense.amount}</p>
-              <p>{expense.description}</p>
-              <button onClick={() => deleteExpense(expense.id)}>Удалить трату</button>
-            </li>
-          ))}
-        </ul>
-      )
-    }
-  </>
-)
-
+  return (
+    <>
+      <h1>История трат:</h1>
+      {expenses.length === 0 
+        ? (<p>Пока что трат не было</p>)
+        : (
+          <>
+            <div className="expense-list-header">
+              <p>Категория</p>
+              <p>Сумма</p>
+              <p>Описание</p>
+              <p>Действие</p>
+            </div>
+            <ul>
+              {expenses.map((expense) => (
+                <li key={expense.id}>
+                  <p>{expense.category}</p>
+                  <p>{expense.amount}</p>
+                  <p>{expense.description}</p>
+                  <button onClick={() => deleteExpense(expense.id)}>Удалить трату</button>
+                </li>
+              ))}
+            </ul>
+          </>
+        )
+      }
+    </>
+  )
 }
 
 export default ExpenseList;
