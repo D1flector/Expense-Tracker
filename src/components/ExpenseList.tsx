c7277c3bab29e69fa83d110f93c37d4c39e33ce0
Dmitry <dima.kornakow@yandeex.ru>
2025-09-15 13:58:28 +0300

    feat: добавлена сортировка по цене

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -1,13 +1,38 @@
+import { useState, useMemo } from "react";
 import useExpenseStore from "../store/useExpenseStore";
 import '../styles/ExpenseList.scss';
 import ExpenseItem from "./ExpenseItem";
 
 const ExpenseList = () => {
   const expenses = useExpenseStore(state => state.expenses);
+  const [sortOrder, setSortOrder] = useState('default');
+
+  function handleOrder(e: React.ChangeEvent<HTMLSelectElement>) {
+    setSortOrder(e.target.value);
+  }
+
+  const sortedExpenes = useMemo(() => {
+    const expensesCopy = [...expenses];
+    if (sortOrder === 'ascending') {
+      expensesCopy.sort((asc, desc) => asc.amount - desc.amount)
+    } else if (sortOrder === 'descending') {
+      expensesCopy.sort((asc, desc) => desc.amount - asc.amount)
+    }
+    return expensesCopy;
+  }, [expenses, sortOrder]);
+
+
 
   return (
     <div className="expense-list-wrapper"> 
       <h2>История трат:</h2>
+      <div className="select-wrapper">
+        <select onChange={handleOrder} value={sortOrder} className="custom-select">
+          <option value="default">Сортировка: по умолчанию</option>
+          <option value="ascending">Сортировка: сначала дешевые</option>
+          <option value="descending">Сортировка: сначала дорогие</option>
+        </select>
+      </div>
       {expenses.length === 0 
         ? (<p className="empty-message">Пока что трат не было</p>)
         : (
@@ -19,7 +44,7 @@ const ExpenseList = () => {
               <p>Действие</p>
             </div>
             <ul className="expense-list">
-              {expenses.map((expense) => (
+              {sortedExpenes.map((expense) => (
                 <ExpenseItem key={expense.id} expense={expense} />
               ))}
             </ul>
